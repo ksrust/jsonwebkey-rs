d7806de31c0762c2d60c6907a76abf1d46f9d97b
ksdev <ksdev@xp.com>
2023-05-03 13:37:14 +0800

    fix: remove lifetime

--- jsonwebkey-convert/src/jwt.rs
+++ jsonwebkey-convert/src/jwt.rs
@@ -2,11 +2,11 @@ use super::*;
 use jsonwebtoken::DecodingKey;
 
 pub trait ToDecodingKey {
-    fn to_decoding_key(&'_ self) -> DecodingKey<'_>;
+    fn to_decoding_key(&'_ self) -> DecodingKey;
 }
 
 impl ToDecodingKey for RSAPublicKey {
-    fn to_decoding_key(&'_ self) -> DecodingKey<'_> {
+    fn to_decoding_key(&'_ self) -> DecodingKey {
         DecodingKey::from_rsa_components(&self.n.base64, &self.e.base64)
     }
 }
